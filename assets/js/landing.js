aa36651a043a60beebf8a3b97e5e532ea85718ca
sami10015 <sami.ellougani1@marist.edu>
2017-08-14 12:44:20 -0400

    NEW WEBSITE VERSION

---
+++ assets/js/landing.js
@@ -0,0 +1,39 @@
+var vid = document.getElementById("bgvid");
+var pauseButton = document.getElementById("pauseButton")
+
+if (window.matchMedia('(prefers-reduced-motion)').matches) {
+    vid.removeAttribute("autoplay");
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+}
+
+function vidFade() {
+  vid.classList.add("stopfade");
+}
+
+// vid.addEventListener('ended', function() {
+//     // only functional if "loop" is removed 
+//     vid.pause();
+//     // to capture IE10
+//     vidFade();
+// }); 
+
+
+pauseButton.addEventListener("click", function() {
+  vid.classList.toggle("stopfade");
+  if (vid.paused) {
+    vid.play();
+    pauseButton.innerHTML = "Pause";
+  } else {
+    vid.pause();
+    pauseButton.innerHTML = "Paused";
+  }
+})
+
+portfolioButton.addEventListener("click", function() {
+  window.location.href='portfolio.html'
+})
+
+resumeButton.addEventListener("click", function() {
+  window.location.href='assets/pdf/Ellougani,Sami-resume.pdf';
+})
